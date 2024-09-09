c3b3d8b3a37e3bd7b829a833db312d58f7deca4f
A-A-RON-SHELBY <aaron.shelby@ucas-edu.net>
2024-09-09 13:07:46 -0600

    Update test.c

--- test.c
+++ test.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(void){
-    printf("Hello World");
+    printf("Aaron");
+    printf("7");
+    printf("700");
+    printf("Toast");
+    printf("Purple");
+    printf("Utah County Academy of Sciences");
+    printf("2024");
+    printf("Brown");
+    printf("14");
+    printf("Geography");
     return 0;
 }
