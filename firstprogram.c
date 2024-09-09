33570e81a43c848969f6ee71e14943e8ffb4a1e5
A-A-RON-SHELBY <aaron.shelby@ucas-edu.net>
2024-09-09 13:35:09 -0600

    Made a whole input

--- firstprogram.c
+++ firstprogram.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
-
-intmain(void){
-  printf("Hello World!");
+ht#include <stdio.h>
+intmain(void) {
+  char name [30];
+  printf("What is your name: \n");
+  scanf("%s", &name);
+  printf("Hello %s, name);
     return 0;
 }
