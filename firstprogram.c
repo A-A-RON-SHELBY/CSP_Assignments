be6b4b18b4f9f47b2c8e91beb1ab59eef67b9c93
A-A-RON-SHELBY <aaron.shelby@ucas-edu.net>
2024-09-10 13:04:50 -0600

    Made a variable

--- firstprogram.c
+++ firstprogram.c
@@ -1,6 +1,8 @@
 ht#include <stdio.h>
 intmain(void) {
+  introduction = ("Hello!");
   char name [30];
+printf("introduction");
   printf("What is your name: \n");
   scanf("%s", &name);
   printf("Hello %s, name);
