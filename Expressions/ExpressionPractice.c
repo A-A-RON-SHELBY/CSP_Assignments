77e882f1b61354ab3a8133b1f47f43834a55843c
A-A-RON-SHELBY <aaron.shelby@ucas-edu.net>
2024-09-19 03:59:38 +0000

    started working on expressionpractice.c

---
+++ Expressions/ExpressionPractice.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    char equation [30];
+    float equation = ("7-24/8*4+6");
+    printf("equation");
+    return 0;
+}
--- firstprogram.c
+++ firstprogram.c
@@ -5,6 +5,6 @@ intmain(void) {
 printf("introduction");
   printf("What is your name: \n");
   scanf("%s", &name);
-  printf("Hello %s, name);
+  printf("Hello %s, name");
     return 0;
 }
