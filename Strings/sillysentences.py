a68021cc54b6636edfe3db28f3155837f56b547b
A-A-RON-SHELBY <aaron.shelby@ucas-edu.net>
2024-09-24 19:31:48 +0000

    Finished sillystentences.py

---
+++ Strings/sillysentences.py
@@ -0,0 +1,7 @@
+animal = input("Name an animal: ")
+place = input("Name an place: ")
+verb = input("Name an verb: ")
+sentenceOne = (" went to the ")
+sentenceTwo = (" and ")
+sentenceThree = ("with his friend the hangry tortoise.")
+print(animal, sentenceOne, place, sentenceTwo, verb, sentenceThree)
