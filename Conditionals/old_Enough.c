188d6d3497ae10d87ef144ea402ab1bbd8ed420f
A-A-RON-SHELBY <aaron.shelby@ucas-edu.net>
2024-10-01 19:13:08 +0000

    made and finished old enough for c

---
+++ Conditionals/old_Enough.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+int age = 100;
+bool vote = true;
+int main(void){
+    if (age >= 18 && vote != false){
+        printf("You can vote!\n");
+    }else if (age >= 16){
+        printf("You are old enough to drive!\n");
+    }else if (age >= 5){
+        if (age == 15){
+            printf("You are old enough to get a learners permit!\n");
+        }else{
+            printf("You are old enough to go to school!\n");
+        }
+    }else{
+        printf("Sorry you are too young to go to school\n");
+    }
+}
