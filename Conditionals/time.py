1b7f351b65c5d0cebe4de3350bbefb44b4355454
A-A-RON-SHELBY <aaron.shelby@ucas-edu.net>
2024-10-04 05:01:29 +0000

    made more to time.py

--- Conditionals/time.py
+++ Conditionals/time.py
@@ -3,3 +3,10 @@ import datetime
 currenttime = datetime.datetime.now()
 
 print(currenttime.hour)
+
+if currenttime <= 12:
+    print("Good Morning")
+elif currenttime <= 18:
+    print("Good Afternoon")
+else: currenttime
+print("Good Evening")
