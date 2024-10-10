3ce2ce14b4c843257e2f79b51380298c438f665d
A-A-RON-SHELBY <aaron.shelby@ucas-edu.net>
2024-10-10 19:44:14 +0000

    made and finished fizz

---
+++ Loops/familyLoop.py
@@ -0,0 +1,5 @@
+family = ("TJ", "Nicole", "Hannah", "Adam", "Caleb", "Aaron")
+count = 0
+while count < 7:
+    count+=1
+    print("Hello", family)
---
+++ Loops/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+ly = ("TJ", "Nicole", "Hannah", "Adam", "Caleb", "Aaron")
+count = 0
+while count < 7:
+    count+=1
+    print("Hello", family)
